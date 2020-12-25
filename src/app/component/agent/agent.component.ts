6ffba7ec6a3ee23bf32a0c57fb0e94bc3ef748e9
babaralishah <babar.bscs3261@iiu.edu.pk>
2020-12-25 16:09:51 +0500

    visualization updated

--- src/app/component/agent/agent.component.ts
+++ src/app/component/agent/agent.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { RestService } from "src/app/Services/rest.service";
+import { FileholderService } from "src/app/Services/fileholder.service";
 
 @Component({
-  selector: 'app-agent',
-  templateUrl: './agent.component.html',
-  styleUrls: ['./agent.component.css']
+  selector: "app-agent",
+  templateUrl: "./agent.component.html",
+  styleUrls: ["./agent.component.css"],
 })
 export class AgentComponent implements OnInit {
+  backendData: any;
 
-  constructor() { }
+  constructor(
+    private FileholderService: FileholderService,
+    private restservice: RestService
+  ) {}
 
   ngOnInit(): void {
+    this.loadVisualzeData();
   }
+  loadVisualzeData() {
+    this.restservice.readResults().subscribe(
+      (data) => {
+        this.backendData = data;
+        console.log(this.backendData);
 
+        // var obj = JSON.parse(this.backendData);
+        // console.log('Backend Data Json parse: ', this.backendData[0]);
+      },
+      (error) => {
+        console.log("No Data Found of Visualization" + error);
+      }
+    );
+  }
 }
