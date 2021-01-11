924cccebd5903c56d9d795f0e0ad71c54b93c6e4
babaralishah <babar.bscs3261@iiu.edu.pk>
2021-01-11 23:17:16 +0500

    visualization changes

--- src/app/component/maincomponent/visualization/visualization.component.ts
+++ src/app/component/maincomponent/visualization/visualization.component.ts
@@ -105,7 +105,6 @@ export class VisualizationComponent implements OnInit {
   sendData() {
     let firstDataColumn: any = [];
     let lastDataColumn: any = [];
-<<<<<<< HEAD
     this.restservice
       .dataFileDetail({ name1: this.name1 })
       .subscribe((data: any) => {
@@ -126,118 +125,5 @@ export class VisualizationComponent implements OnInit {
         // console.log(this.doughnutChartLabels);
         // console.log(this.doughnutChartData);
       });
-=======
-    for (let i = 0; i < 50; i++) {
-      this.lastDataColumn.forEach(function (data: any) {
-        lastDataColumn[i] = data[0];
-        // console.log(lastDataColumn[i]);
-      });
-    }
-    for (let i = 0; i < 50; i++) {
-      firstDataColumn[i] = this.firstDataColumn[i];
-    }
-    this.doughnutChartLabels = firstDataColumn;
-    this.doughnutChartData = lastDataColumn;
-    this.pieChartData = lastDataColumn;
-    this.pieChartLabels = firstDataColumn;
-    console.log(this.doughnutChartLabels);
-    console.log(this.doughnutChartData);
-    // this.restservice
-    //   .dataFileDetail({ name1: this.name1, name2: this.name2 })
-    //   .subscribe((data: any) => {
-    //     console.log(data);
-
-    //     const object1 = JSON.parse(data[1]);
-    //     const object2 = JSON.parse(data[2]);
-    //     const arr1: any[] = [];
-    //     const arr2 = [];
-    //     this.chartname = "bar";
-
-    //     let count = 0;
-    //     for (const property in Object.values(object1)) {
-    //       const obj = { cat: null, length: 0, avg: 0 };
-    //       if (!arr1.includes(object1[property])) {
-    //         arr1.push(object1[property]);
-    //         obj.cat = object1[property];
-    //         obj.length++;
-    //         obj.avg = object2[property];
-    //         arr2.push(obj);
-    //       } else {
-    //         for (const arr of arr2) {
-    //           if (arr.cat == object1[property]) {
-    //             const sum = arr.avg * arr.length + object2[property];
-    //             arr.length++;
-    //             arr.avg = sum / arr.length;
-    //           }
-    //         }
-    //       }
-    //       count++;
-    //     }
-
-    //     const arr = data[0].split("\n");
-    //     const catArr: any[] = [],
-    //       lengthArr: any[] = [],
-    //       avgArr: any[] = [];
-    //     arr.forEach((element: string) => {
-    //       const dataSplit = element.split(" ");
-
-    //       catArr.push(dataSplit[0]);
-    //       lengthArr.push(dataSplit[dataSplit.length - 1]);
-    //     });
-    //     arr2.forEach((element) => {
-    //       if (avgArr.length >= 10) {
-    //         return;
-    //       }
-    //       avgArr.push(element.avg);
-    //     });
-    //     console.log(catArr);
-    //     console.log(lengthArr);
-    //     console.log(avgArr);
-
-    //     setTimeout(() => {
-    //       const labels = [];
-    //       const complexArr = [];
-    //       for (let i = 0; i < lengthArr.length; i++) {
-    //         if (i >= 10) {
-    //           break;
-    //         }
-    //         const label = catArr[i] + " - " + lengthArr[i];
-    //         labels.push(label);
-
-    //         const innerData = { data: [], label: catArr[i] };
-    //         for (let j = 0; j < lengthArr.length; j++) {
-    //           // innerData.data.push(0);
-    //         }
-    //         // innerData.data.splice(i, 1, avgArr[i]);
-    //         complexArr.push(innerData);
-    //       }
-    // console.log(complexArr);
-    // console.log(avgArr);
-    // console.log(labels);
-    // console.log(complexArr);
-    // this.doughnutChartData = avgArr;
-
-    // this.lineChartLabels = labels;
-    // this.lineChartData = avgArr;
-
-    // this.polarChartLabels = labels;
-    // this.polarChartData = avgArr;
-
-    // this.bubbleChartLabels = labels;
-    // this.bubbleChartData = complexArr;
-
-    // this.pieChartLabels = labels;
-    // this.pieChartData = avgArr;
-
-    // this.barChartLabels = labels;
-    // this.barChartData = complexArr;
-
-    // this.radarChartLabels = labels;
-    // this.radarChartData = complexArr;
-    // this.barChartData = labels;
-    //         console.log("\nDone\n");
-    //       });
-    //     });
->>>>>>> c14cd1ccfb7bb54999831522d953bf6b42f0346d
   }
 }
